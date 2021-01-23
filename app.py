685f412ed1fe082df0cbef352f1f31db2f3b509c
chunyu618 <b06902048@ntu.edu.tw>
2021-01-23 23:45:12 +0800

    testtest

--- app.py
+++ app.py
@@ -40,9 +40,9 @@ def handle_message(event):
     ID = ""
     reply = ""
     if event.source.type == "group":
-        ID = event.source.groupId
+        ID = "Ccfb3d059fffde96fcab318e1c5a24c7e"
     elif event.source.type == "user":   
-        ID = event.source.userId
+        ID = event.source.user_id
     if "生日快樂" in message.text:
         reply = TextSendMessage(text="@林珺瑩 生日快樂")
     #if "切" in message.text:
