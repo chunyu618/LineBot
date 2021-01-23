40d2c4fc26b5532f6f48da4794611235396c3b67
chunyu618 <b06902048@ntu.edu.tw>
2021-01-23 22:50:59 +0800

    testtest

--- app.py
+++ app.py
@@ -36,6 +36,8 @@ def handle_message(event):
     message = TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(event.reply_token, message)
     reply = ""
+    printf(message)
+    printf("生日快樂" in message)
     if "生日快樂" in message:
         line_bot_api.reply_message(event.reply_token, message)
         reply = TextSendMessage(text="@林珺瑩 生日快樂")
