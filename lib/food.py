2206e077b36305673fe053b029bf86c731e1f641
chunyu618 <b06902048@ntu.edu.tw>
2021-01-24 14:07:06 +0800

    food recommend

---
+++ lib/food.py
@@ -0,0 +1,26 @@
+import json
+from random import seed
+from random import random
+from random import sample
+from random import randint
+from datetime import datetime
+seed(datetime.now())
+
+def getFood():
+    rev = ""
+    with open('files/food.json') as f:
+        data = json.load(f)
+        #print(data)
+        arr = sample(range(len(data)), 3)
+        key = list(data.keys())
+        for foodIndex in arr:
+            index = randint(0, len(data[key[foodIndex]]) - 1)
+            rev += "%s: %s\n" % (key[foodIndex], data[key[foodIndex]][index])
+
+    return rev    
+
+def main():
+    print(getFood())
+
+if __name__ == "__main__":
+    main()
---
+++ test.py
@@ -0,0 +1,4 @@
+from lib.handleReply import getReply
+
+
+print(getReply(input()))
